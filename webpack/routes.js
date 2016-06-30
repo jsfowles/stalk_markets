f98441791b5bcd3dab1d33ba0e78ffa62aee37f3
Jacob Fowles <js.fowles@gmail.com>
2016-06-30 15:46:12 -0600

    added market into routes.js

--- webpack/routes.js
+++ webpack/routes.js
@@ -2,7 +2,7 @@ import React from 'react';
 import { Route, IndexRoute } from 'react-router';
 import App from './containers/App';
 import NoMatch from './components/NoMatch';
-
+import Markets from './components/Market';
 import Vendors from './components/Vendors';
 import Vendor from './components/Vendor';
 
@@ -11,7 +11,8 @@ export default (
     <Route path="/" component={App} >
     <Route path="/vendors" component={Vendors} />
     <Route path="/vendors/:id" component={Vendor} />
-
+    <Route path="/markets" component={Markets} />
+    <Route path="/markets/:id" component={Market} />
     </Route>
     <Route path="*" status={404} component={NoMatch}/>
   </Route>
