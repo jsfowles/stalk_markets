f808a5ed90488f2275f1853982aa90068b14c1f0
Mara Behles Dean <maraebehles@gmail.com>
2016-07-07 17:24:36 -0600

    seeded vendors

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,3 +1,14 @@
 include Sprig::Helpers
 
 sprig [Market]
+
+10.times do |n|
+  Vendor.create(
+    first_name: Faker::Name.name,
+    last_name: Faker::Name.name,
+    business_name: Faker::Company.name,
+    contact_phone: Faker::PhoneNumber.phone_number,
+    contact_email: Faker::Internet.email,
+    website_link: Faker::Internet.url,
+    vendor_type: Faker::Commerce.department)
+end
