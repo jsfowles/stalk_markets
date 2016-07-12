879fe1deff4b72599e00d57c44cc2e65d9c82e65
Faith Oladele <faitholadele@gmail.com>
2016-07-12 10:12:28 -0600

    map updated

--- webpack/components/SimpleMap.jsx
+++ webpack/components/SimpleMap.jsx
@@ -4,9 +4,11 @@ import {GoogleMapLoader, GoogleMap, Marker} from "react-google-maps";
 class SimpleMap extends React.Component{
   constructor(props) {
     super(props);
+    this.renderMap = this.renderMap.bind(this);
   }
 
-  render() {
+
+  renderMap() {
     let market = this.props.market;
     let markers = [];
     if(market.latitude && market.longitude) {
@@ -18,7 +20,9 @@ class SimpleMap extends React.Component{
         key: `market-${market.id}`,
         defaultAnimation: 2,
       }]
-    }
+
+    let lat = market.latitude || 40.760779;
+    let lng = market.longitude || -111.891047;
     return (
       <GoogleMapLoader
           containerElement={
@@ -30,8 +34,8 @@ class SimpleMap extends React.Component{
           googleMapElement={
             <GoogleMap
               ref={(map) => (this._googleMapComponent = map)}
-              defaultZoom={3}
-              defaultCenter={{ lat: 40.760779, lng: -111.891047 }}
+              defaultZoom={10}
+              defaultCenter={{ lat, lng }}
             >
             {markers.map((marker, index) => {
               return (
@@ -44,6 +48,15 @@ class SimpleMap extends React.Component{
           }
         />
       );
+    }
+  }
+
+  render() {
+    return (
+      <div>
+        {this.renderMap()}
+      </div>
+    )
   }
 }
 
