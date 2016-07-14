8dd179803eb5c3f058e89d9762dc825bbe65ca9a
Faith Oladele <faitholadele@gmail.com>
2016-07-14 04:51:07 -0600

    Add not fully functional, its still buggy

--- webpack/components/AddVendorMarket.jsx
+++ webpack/components/AddVendorMarket.jsx
@@ -14,8 +14,6 @@ class AddVendorMarket extends React.Component {
     	selectedMarkets.push($(this).val());
 		});
 		
-		// const market_id = '';
-
 	  $.ajax({
 	  	url: `/api/join_tables`,
 	  	type: 'POST',
@@ -29,19 +27,19 @@ class AddVendorMarket extends React.Component {
 	}
 
 	render() {
+		let checkboxes = [];
 		let markets = this.props.markets.map( market => {
 			let tables = this.props.joinTable.map( table => {
-				let checked = table.market_id == market.id;
 				let key = `market-id${market.id}`
-				if( checked ) {
-					return( 
+				if( table.market_id === market.id ) {
+					checkboxes.push( 
 						<div key={key}>
-				  		<input name="markets" type="checkbox" id={market.id} value={market.id} />
+				  		<input name="markets" type="checkbox" id={market.id} value={market.id} defaultVaule="true" />
 				  		<label htmlFor={market.id}>{market.name}</label>
 				  	</div> 
 					)
 				} else {
-				  return ( 
+				  checkboxes.push ( 
 				  	<div key={key}>
 				  		<input name="markets" type="checkbox" id={market.id} value={market.id} />
 				  		<label htmlFor={market.id}>{market.name}</label>
@@ -49,10 +47,10 @@ class AddVendorMarket extends React.Component {
 				  	)
 			  }
 			})
-
 		});
 		return(
 			<div>
+			  {checkboxes}
 				<form ref='vendorForm' onSubmit={(e) => this.submitMarket(e)} >
 					{markets}
 					<input type='submit' className='btn' value='Add Markets' />
