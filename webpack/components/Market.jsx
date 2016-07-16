75f91816ad4ecb9073f1396b3b5ad5c7ec263968
Jacob Fowles <js.fowles@gmail.com>
2016-07-16 12:46:35 -0600

    changed market order to prepare for checkboxes

--- webpack/components/Market.jsx
+++ webpack/components/Market.jsx
@@ -25,6 +25,7 @@ class Market extends React.Component {
 	}
 
 	render() {
+
 		return(
 			<div className='row market'>
 				<div className='col s12 m4 offset-m4 market-margin'>
@@ -38,20 +39,22 @@ class Market extends React.Component {
 				<div className="col s12 m6 offset-m4 market-margin">
 					<div className="white-text">
 						<div className='row col s12 m12 l12'>
-							<div className='col s12 m4 offest-m3'>
+							<div className='col s12 m4 offest-m3 market-address'>
 								<p>Address:</p>
 								<p>{this.state.market.address}</p>
 								<p>{this.state.market.city + ", " + this.state.market.state + " " + this.state.market.zip}</p>
 							</div>
-							<div className='col s12 m1 offest-m3'>
+							<div className='col s6 m6 market-hours'>
 								<p>{"Season Open: " + this.state.market.start_date + "  -  " + this.state.market.end_date}</p>
 								<p>{"Hours: " + this.state.market.start_time + "  -  " + this.state.market.end_time}</p>
 								<p>{"Days Open: " + this.state.market.day_of_week}</p>
+								<p>Contact Phone: <a href={"tel:" + this.state.market.contact_phone}>{this.state.market.contact_phone}</a></p>
 							</div>
-							<div className='container'>
-								<p>{"Contact Phone: " + this.state.market.contact_phone}</p>
+							<div className='col s6 m4 market-extras'>
 								<p>{"Pet Friendly: " + this.state.market.pet_friendly}</p>
 								<p>{"Crafts: " + this.state.market.crafts}</p>
+							</div>
+							<div className='col s3 m6 '>
 								<p>{"Alcohol Served: " + this.state.market.alcohol}</p>
 								<p>{"Prepared Foods: " + this.state.market.prepared_food}</p>
 							</div>
