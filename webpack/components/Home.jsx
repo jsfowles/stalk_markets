e319d7ac08fca1f7e72d9f3ed6f138b118f27ef9
Taylor Wu <Taylorwu21@gmail.com>
2016-07-16 16:52:35 -0600

    styled home and stuff

--- webpack/components/Home.jsx
+++ webpack/components/Home.jsx
@@ -5,14 +5,14 @@ import { Link } from 'react-router'
 const Home = () => (
   <div>
     <div className='row home-row'>
-      <div className="col s12 m6">
-        <div className="home-div-vendors valign-wrapper">
-          <a className='valign white-text home-vendors-link' href="/vendors">VENDORS</a>
+      <div className="col s12 m12">
+        <div className="home-div-markets valign-wrapper home-text-size">
+          <a className='valign white-text home-markets-link' href="/markets">MARKETS</a>
         </div>
       </div>
-      <div className="col s12 m6">
-        <div className="home-div-markets valign-wrapper">
-          <a className='valign white-text home-markets-link' href="/markets">MARKETS</a>
+      <div className="col s12 m12">
+        <div className="home-div-vendors valign-wrapper home-text-size">
+          <a className='valign white-text home-vendors-link' href="/vendors">VENDORS</a>
         </div>
       </div>
     </div>
