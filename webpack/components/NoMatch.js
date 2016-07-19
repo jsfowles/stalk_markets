f7465fda597f215c004633db82e53d3afeec9fa4
Jacob Fowles <js.fowles@gmail.com>
2016-07-19 16:37:02 -0600

    updated 404 page

--- webpack/components/NoMatch.js
+++ webpack/components/NoMatch.js
@@ -1,8 +1,14 @@
 import React from 'react';
 
 const NoMatch = () => (
-  <div>404</div>
+  <div className='container'>
+  <div><img src="https://res.cloudinary.com/jsfowles/image/upload/v1468708498/stalkmarket-white_pc4som.png" className='responsive-img'/></div>
+  <div className='center'>
+    <h1>404</h1>
+    <h4>Oops! We can't seem to find the page you're looking for.</h4>
+    <h4>Click to come back <a href='/' className='btn'>Home</a></h4>
+  </div>
+  </div>
 )
 
 export default NoMatch;
-
