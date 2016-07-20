b7c40ddc7bae7ca35d4a8fb1073939d12f7fcde5
Taylor Wu <Taylorwu21@gmail.com>
2016-07-20 12:13:08 -0600

    fixed github kitties

--- webpack/components/AboutUs.jsx
+++ webpack/components/AboutUs.jsx
@@ -26,14 +26,14 @@ const AboutUs = () => (
 		      <div className="col s12 m6 center team-margin">
 						<img src="https://res.cloudinary.com/jsfowles/image/upload/v1468734687/IMG_8309_tnb2g5.jpg" className='profile-picture responsive-image' alt="Mara" />
 						<h4 className="s12 m6">Mara Behles Dean</h4>
-						<a href="https://github.com/marabd" target="_blank"><img src="https://camo.githubusercontent.com/d934489ecd7f4e10b556ea93f19e235ae33de17b/687474703a2f2f6f63746f6465782e6769746875622e636f6d2f696d616765732f6461667470756e6b746f6361742d6775792e676966" className='self-github' alt="Github" /></a>
+						<a href="https://github.com/marabd" target="_blank"><img src="https://res.cloudinary.com/jsfowles/image/upload/v1469036574/daftpunktocat-guy_qhautr.gif" className='self-github' alt="Github" /></a>
 						<a href="https://www.linkedin.com/in/marabd/en" target="_blank"><img src="http://res.cloudinary.com/jsfowles/image/upload/v1467837846/About/linkedin-icon_dbu1no.png" className='linkedin-icon' alt="Mara Behles Dean's Linkedin" /></a>
 						<p className="container">I am a Chicagoland native, University of Wisconsin at Madison grad, and most recently, a web developer. Although I haven't been coding very long, I am very passionate about it and love the challenges that go along with programming. I love the field and am excited for a career in web development. Offline I love to cook, be outdoors, travel, make and enjoy a good cocktail, and eat all the cheese.</p>
 		      </div>
 		      <div className="col s12 m6 center team-margin">
 						<img src="https://res.cloudinary.com/jsfowles/image/upload/v1468734688/IMG_8305_sues4j.jpg" className='profile-picture responsive-image' alt="Taylor" />
 						<h4>Taylor Wu</h4>
-						<a href="https://github.com/TaylorWu21" target="_blank"><img src="https://camo.githubusercontent.com/d5b74a6ac7e23270fb39582195a377b13cf10f3d/687474703a2f2f6f63746f6465782e6769746875622e636f6d2f696d616765732f6461667470756e6b746f6361742d74686f6d61732e676966" className='self-github' alt="Github" /></a>
+						<a href="https://github.com/TaylorWu21" target="_blank"><img src="https://res.cloudinary.com/jsfowles/image/upload/v1469036582/daftpunktocat-thomas_kyg38c.gif" className='self-github' alt="Github" /></a>
 						<a href="https://www.linkedin.com/in/taylorswu" target="_blank"><img src="http://res.cloudinary.com/jsfowles/image/upload/v1467837846/About/linkedin-icon_dbu1no.png" className='linkedin-icon' alt="Taylor Wu's Linkedin" /></a>
 						<p className="container">I am currently a senior at the University of Utah. I will graduate in spring of 2017 with a BS degree in Information Systems. The summer of 2016 is where I found my passion for programming. Having no prior experience with programming before, I have learned to enjoy the struggle of being a programmer. I hope my understanding of programming will be an integral part of my professional life and hopefully I will be able to create a career out of it. During my free time I love to play video games and snowboard!</p>
 		      </div>
@@ -42,14 +42,14 @@ const AboutUs = () => (
 		      <div className="col s12 m6 center team-margin">
 						<img src="https://res.cloudinary.com/jsfowles/image/upload/v1468734688/IMG_8296_fbaqfm.jpg" className='profile-picture responsive-image' alt="Faith" />
 						<h4 className="s12 m6">Faith Oladele</h4>
-						<a href="https://github.com/foladele" target="_blank"><img src="https://res.cloudinary.com/jsfowles/image/upload/v1467837826/About/octocat_giq7ew.png" className='self-github'alt="Github" /></a>
+						<a href="https://github.com/foladele" target="_blank"><img src="https://res.cloudinary.com/jsfowles/image/upload/v1469036679/topguntocat_qa88oa.png" className='self-github'alt="Github" /></a>
 						<a href="https://www.linkedin.com/in/foladele" target="_blank"><img src="http://res.cloudinary.com/jsfowles/image/upload/v1467837846/About/linkedin-icon_dbu1no.png" className='linkedin-icon'alt="Faith Oladele's Linkedin" /></a>
 						<p className="container">Born in Nigeria West Africa and currently attending the University of Utah as a Computer Science Major. I am also in the Air Force Reserves as a Traffic Management officer, and work for JC Penny Shared Services. I love to read, sew, cook and I enjoy learning new programming languages.</p>
 		      </div>
 		      <div className="col s12 m6 center team-margin">
 						<img src="https://res.cloudinary.com/jsfowles/image/upload/v1468734687/IMG_8318_wl17an.jpg" className='profile-picture responsive-image'alt="Jacob" />
 						<h4>Jacob Fowles</h4>
-						<a href="https://github.com/jsfowles" target="_blank"><img src="https://res.cloudinary.com/jsfowles/image/upload/v1468958450/oktobercat_ifrw9r.png" className='self-github' alt="Github" /></a>
+						<a href="https://github.com/jsfowles" target="_blank"><img src="https://res.cloudinary.com/jsfowles/image/upload/v1469037002/1_axnytu.png" className='self-github' alt="Github" /></a>
 						<a href="https://www.linkedin.com/in/jsfowles" target="_blank"><img src="https://res.cloudinary.com/jsfowles/image/upload/v1467837846/About/linkedin-icon_dbu1no.png" className='linkedin-icon' alt="Jacob Fowles's Linkedin" /></a>
 						<p className="container">Once a professional guitarist and banker, now following my passion for software engineering.  I enjoy all thing related to AI and the neurosciences, especially learning machines. Lover of photography, fishing, and basically anything with an engine.</p>
 		      </div>
