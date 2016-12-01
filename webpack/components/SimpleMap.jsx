03353bf9e3cba65e305613f28123adea02fb48b2
jsfowles <js.fowles@gmail.com>
2016-12-01 15:46:50 -0700

    fixing another typo?

--- webpack/components/SimpleMap.jsx
+++ webpack/components/SimpleMap.jsx
@@ -25,36 +25,36 @@ class SimpleMap extends React.Component{
     let lng = market.longitude || -111.891047;
     return (
       <GoogleMapLoader
-          containerElement={
-            <div
-              {...this.props}
-              className='google-map'
-            />
-          }
-          googleMapElement={
-            <GoogleMap
-              ref={(map) => (this._googleMapComponent = map)}
-              defaultZoom={15}
-              defaultCenter={{ lat, lng }}
-              scrollwheel={false}
-              draggable={false}
-              scaleControl={false}
-              navigationControl={false}
-              mapTypeControl={false}
-              zoomControl={false}
-              disableDoubleClickZoom={true}
-            >
+        containerElement={
+          <div
+            {...this.props}
+            className='google-map'
+          />
+        }
+        googleMapElement={
+          <GoogleMap
+            ref={(map) => (this._googleMapComponent = map)}
+            defaultZoom={15}
+            defaultCenter={{ lat, lng }}
+            scrollwheel={false}
+            draggable={false}
+            scaleControl={false}
+            navigationControl={false}
+            mapTypeControl={false}
+            zoomControl={false}
+            disableDoubleClickZoom={true}
+          >
 
-            {markers.map((marker, index) => {
-              return (
-                <Marker
-                  {...marker}
-                />
-              );
-            })}
-            }
-            </GoogleMap>
-        />
+          {markers.map((marker, index) => {
+            return (
+              <Marker
+                {...marker}
+              />
+            );
+          })}
+          }
+          </GoogleMap>
+        }
       );
     }
   }
